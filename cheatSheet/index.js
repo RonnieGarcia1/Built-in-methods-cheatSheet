e81b52d26c05b8e699aed607d5eb5a04ff4b5734
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-08-27 10:39:28 -0400

    Removed console logs

--- cheatSheet/index.js
+++ cheatSheet/index.js
@@ -1,19 +1,19 @@
 //NUMBERS
 
 //Number.parseInt() - converting number string into a number.
-console.log(Number.parseInt("76")); //> 76
+Number.parseInt("76"); //> 76
 
 //Number.parseFloat() - Allows decimal place and returns a number.
-console.log(Number.parseFloat("76.01")); //> 76.01
+Number.parseFloat("76.01"); //> 76.01
 
 //Number.isNan() - Allows us to check for numbers.
 Number.isNaN(NaN); //> true
-console.log(Number.isNaN(76)); //> false
+Number.isNaN(76); //> false
 
 //Math.ceil() -  gets the highest value from a number with decimal values.
 Math.ceil(76.9); //> 77
 Math.ceil(76.1); //> 77
-console.log(Math.ceil(76.008756348756239001)); //> 77
+Math.ceil(76.008756348756239001); //> 77
 
 
 //MATH 
@@ -21,19 +21,19 @@ console.log(Math.ceil(76.008756348756239001)); //> 77
 //.floor() - gets the lowest value from a number with decimal values.
 Math.floor(76.1); //> 76
 Math.floor(76.9); //> 76
-console.log(Math.floor(76.9994385729384759999)); //> 76
+Math.floor(76.9994385729384759999); //> 76
 
 //Math.round() - will round a number to the nearest integer.
 Math.round(76.1); //> 76
 Math.round(76.5); //> 77
-console.log(Math.round(76.95398573987)); //> 77
+Math.round(76.95398573987); //> 77
 
 //Math.max() - takes in any amount of numbers and returns the highest one.
-console.log(Math.max(10, -1, 76, 11)); //> 76
+Math.max(10, -1, 76, 11); //> 76
 
 //Math.min() - takes in any amount of numbers and returns the lowest one.
 Math.min(10, -1, 76, 11); //> -1
-console.log(Math.min(10, "A")); //> NaN
+Math.min(10, "A"); //> NaN
 
 //Math.random() - number returns a random value between 0 and 1.
 Math.random() //> 0.01014371907929501
@@ -53,11 +53,11 @@ Math.floor(Math.random() * 10);
 Math.ceil(Math.random() * 4);
 
 //> returns a number between 0 - 10
-console.log(Math.floor(Math.random() * 11));
+Math.floor(Math.random() * 11);
 
 //> returns a number between a range of two numbers
 
-console.log(Math.ceil(Math.random() * 10)+ 10) // adds to the value being multiplied to meet the max value in the range
+Math.ceil(Math.random() * 10)+ 10 // adds to the value being multiplied to meet the max value in the range
 
 
 //Objects
@@ -66,7 +66,7 @@ console.log(Math.ceil(Math.random() * 10)+ 10) // adds to the value being multip
 
 // Object.values() -  returns an array of all the values of the given object.
 
-console.log(Object.values({ heads: 10, tails: 9 })); //> [ 10, 9 ];
+Object.values({ heads: 10, tails: 9 }); //> [ 10, 9 ];
 
 // It only returns the values at the first level.
 
@@ -81,12 +81,12 @@ Object.values({flips: {
 Object.keys({ heads: 10, tails: 9 });
 //> [ "heads", "tails" ];
 
-console.log(Object.keys({
+Object.keys({
   flips: {
     heads: 10,
     tails: 9,
   },
-})); //> [ "flips" ]
+}); //> [ "flips" ]
 
 // It only returns the keys at the first level.
 
@@ -121,69 +121,66 @@ Array.isArray(["heads", "tails"]); //> true
 
 //Lesson Notes
 
-/* Learning Objectives
-By the end of this lesson you should be able to:
+// Learning Objectives
+// By the end of this lesson you should be able to:
 
-Identify a built-in method.
-Use common Number methods like .parseInt(), .parseFloat(), and .isNaN().
-Use common Math methods like .ceil(), .floor(), .round(), .max(), .min(), and .random().
-Use common Object methods like .values(), .keys(), and .entries().
-Use the common Array.isArray() method.
+// Identify a built-in method.
+// Use common Number methods like .parseInt(), .parseFloat(), and .isNaN().
+// Use common Math methods like .ceil(), .floor(), .round(), .max(), .min(), and .random().
+// Use common Object methods like .values(), .keys(), and .entries().
+// Use the common Array.isArray() method.
 
-Guiding Questions
-What is a built-in method?
+// Guiding Questions
+// What is a built-in method?
 
-Documentation can sometimes be difficult to read, but is essential to your success as a developer. Take a look at the following page from the Mozilla Developer Network that describes the Math.max() method. Then, describe in your own words what the Math.max() method does.
+// Documentation can sometimes be difficult to read, but is essential to your success as a developer. Take a look at the following page from the Mozilla Developer Network that describes the Math.max() method. Then, describe in your own words what the Math.max() method does.
 
-MDN: Math.max()
+// MDN: Math.max()
 
+// Why is the Number.isNaN() method helpful?
 
-Why is the Number.isNaN() method helpful?
+// a: allows us to check if a number is present or not.
 
-a: allows us to check if a number is present or not.
+// Why is the Array.isArray() method helpful?
 
-Why is the Array.isArray() method helpful?
+// a: returns a boolean. this could be helpful in cases where we need to know if there as an array present in our program. 
 
-a: returns a boolean. this could be helpful in cases where we need to know if there as an array present in our program. 
+// What does the Math.random() method do?
 
-What does the Math.random() method do?
+// a: returns a random number between 0 & 1.
 
-a: returns a random number between 0 & 1.
+// How can you return a random number between 0 and 9?
 
-How can you return a random number between 0 and 9?
+Math.floor(Math.random() * 10);
 
-return Math.floor(Math.random() * 10);
+// How can you return a random number between 0 and 99?
 
-How can you return a random number between 0 and 99?
+Math.floor(Math.random() * 100)
 
-return Math.floor(Math.random() * 100)
+// How can you return a random number between 1 and 100?
 
-How can you return a random number between 1 and 100?
+Math.ceil(Math.random() * 100)
 
-return Math.ceil(Math.random() * 100)
+// How can you return a random number between 1 and 50?
 
-How can you return a random number between 1 and 50?
+Math.ceil(Math.random() * 50)
 
-return Math.ceil(Math.random() * 50)
+// How can you return a random number between 10 and 20?
 
-How can you return a random number between 10 and 20?
-
-return Math.ceil(Math.random() * 10)+ 10;
-
+Math.ceil(Math.random() * 10)+ 10;
 
 
-Given an array of integers where some numbers are strings, return the largest integer as a number.
+// Given an array of integers where some numbers are strings, return the largest integer as a number.
 
-e.g. [4, "10", 9]
+// e.g. [4, "10", 9]
 
-*/
 
-function getHigh(highest){
+function largestInteger(highest){
     const map1 = highest.map(x => x * 1);
 
     return Math.max(...map1);
     }
-    console.log(getHigh([4, "10", 9]));
+    console.log(largestInteger([4, "10", 9]));
 
 /*
 Given an object, verify that none of the values of those keys are NaN.
